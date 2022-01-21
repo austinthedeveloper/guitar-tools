5c369b5d204fa4acf2cebc526e2a61d383fceee8
Austin Stewart <hello@austinthedeveloper.com>
2022-01-21 10:42:46 -0600

    Working demo

--- libs/drums/src/lib/components/drum-config/drum-config.component.ts
+++ libs/drums/src/lib/components/drum-config/drum-config.component.ts
@@ -5,32 +5,94 @@ import {
   MIDI_MESSAGES,
   inputById,
   outputByName,
+  MIDI_ACCESS,
+  MIDI_OUTPUT,
 } from '@ng-web-apis/midi';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { filter, first } from 'rxjs/operators';
 import MIDIInput = WebMidi.MIDIInput;
 import MIDIMessageEvent = WebMidi.MIDIMessageEvent;
+import MIDIAccess = WebMidi.MIDIAccess;
+import MIDIOutput = WebMidi.MIDIOutput;
+
+interface DrumKeyPress {
+  key: number;
+  hardness: number;
+  timestamp: number;
+}
 
 @Component({
   selector: 'guitar-drum-config',
   templateUrl: './drum-config.component.html',
   styleUrls: ['./drum-config.component.css'],
-  providers: [
-    inputById('input-0'),
-    // , outputByName('VirtualMIDISynth')
-  ],
+  providers: [inputById('input-0'), outputByName('VirtualMIDISynth')],
 })
 export class DrumConfigComponent implements OnInit {
+  config = {
+    snare: 38,
+    tap: 40,
+    tom1: 48,
+    tom2: 45,
+    tom3: 43,
+    hiHat: 46,
+    ride: 51,
+    crash1: 49,
+  };
+  private sub: BehaviorSubject<DrumKeyPress[]> = new BehaviorSubject(
+    [] as DrumKeyPress[]
+  );
+  inputs$ = this.sub.asObservable();
   constructor(
     @Inject(MIDI_SUPPORT) readonly supported: boolean,
+    @Inject(MIDI_ACCESS) access: Promise<MIDIAccess>,
     @Inject(MIDI_MESSAGES) messages$: Observable<MIDIMessageEvent>,
-    @Inject(MIDI_INPUT) private input: Promise<MIDIInput>
+    @Inject(MIDI_INPUT) readonly input: Promise<MIDIInput>,
+    @Inject(MIDI_OUTPUT) output: Promise<MIDIOutput>
   ) {
-    console.log('hit', messages$, this.input);
+    messages$
+      .pipe(
+        filter((res) => !!(res && res.data.length === 3 && res.data[2] !== 0))
+      )
+      // .pipe(first())
+      .subscribe((res) => {
+        const key = res.data[1];
+        const hardness = res.data[2];
+        this.addInput({
+          key,
+          hardness,
+          timestamp: res.timeStamp,
+        });
+
+        console.log('message sub', res, res.data, res.data.length, {
+          key,
+          hardness,
+          timestamp: res.timeStamp,
+        });
+      });
+    console.log('hit', messages$, input);
+    access.then((res) => {
+      console.log('access', res, res.inputs.values);
+    });
+    input.then(
+      (res) => {
+        console.log('then', res);
+      },
+      (err) => {
+        console.log('thenerr', err);
+      }
+    );
+    output.then((res) => {
+      console.log('thoutputen', res);
+    });
   }
 
   ngOnInit(): void {
     // this.input.then((res) => {
     //   console.log('hit', res);
     // });
+  }
+
+  addInput(test: DrumKeyPress) {
+    this.sub.next([...this.sub.value, test]);
   }
 }
