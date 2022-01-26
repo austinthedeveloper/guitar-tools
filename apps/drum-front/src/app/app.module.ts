7b07eba4a8e684246ffd7c44458b8b79b7b9319d
Austin Stewart <hello@austinthedeveloper.com>
2022-01-25 21:37:08 -0600

    drum app

--- apps/drum-front/src/app/app.module.ts
+++ apps/drum-front/src/app/app.module.ts
@@ -1,19 +1,17 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { DrumsModule } from '@guitar/drums';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { GuitarToolsModule } from '@guitar/guitar-tools';
-
 import { AppComponent } from './app.component';
-import { APP_COMPONENTS } from './components';
 
 @NgModule({
-  declarations: [AppComponent, ...APP_COMPONENTS],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    GuitarToolsModule,
     FormsModule,
     ReactiveFormsModule,
+    DrumsModule,
     BrowserAnimationsModule,
   ],
   providers: [],
