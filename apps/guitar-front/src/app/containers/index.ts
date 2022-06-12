70e1233ca74ce0f9ea78219e1f1ad8524ade958a
Austin Stewart <hello@austinthedeveloper.com>
2022-06-12 13:15:44 -0500

    Added navbar

--- apps/guitar-front/src/app/containers/index.ts
+++ apps/guitar-front/src/app/containers/index.ts
@@ -1,4 +1,5 @@
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { QuizComponent } from './quiz/quiz.component';
 
-export const APP_CONTAINERS = [HomeComponent, QuizComponent];
+export const APP_CONTAINERS = [HomeComponent, QuizComponent, NavbarComponent];
