1675b1305e3197b04699295e494aac1fcaf257b1
Austin Stewart <hello@austinthedeveloper.com>
2022-06-29 23:40:28 -0500

    Triad position 1 bug

--- libs/data/src/lib/triads/diminished/triads-position-1.data.ts
+++ libs/data/src/lib/triads/diminished/triads-position-1.data.ts
@@ -2,7 +2,7 @@ import { scaleStartWith } from '@guitar/helpers';
 import { ChordClass, ChordInterface } from '@guitar/interfaces';
 
 function CHORDS_MOCK_TRIADS_1(): ChordInterface[] {
-  return scaleStartWith('C').map((string, index) => {
+  return scaleStartWith('C# / D♭').map((string, index) => {
     const chords = [
       {
         fret: index.toString(),
@@ -13,7 +13,7 @@ function CHORDS_MOCK_TRIADS_1(): ChordInterface[] {
         string: '4',
       },
       {
-        fret: (index + 3).toString(),
+        fret: (index + 4).toString(),
         string: '5',
       },
     ];
