446b5cb49c84103b3ff00a759af89afb753b2c9e
Austin Stewart <astewart@texasdevs.com>
2025-02-26 23:07:52 -0600

    Mongoose setup

--- apps/guitar-api/src/app/app.module.ts
+++ apps/guitar-api/src/app/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(), // Loads .env file
+    MongooseModule.forRoot(
+      process.env.MONGO_URI || 'mongodb://localhost:27017/mydatabase'
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
