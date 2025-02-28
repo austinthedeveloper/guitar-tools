9595548337ba4d4eebed655a44462b4613414bb6
Austin Stewart <astewart@texasdevs.com>
2025-02-28 14:52:59 -0600

    Removed bad import

--- libs/setup/src/lib/services/pedal-usage.service.ts
+++ libs/setup/src/lib/services/pedal-usage.service.ts
@@ -2,8 +2,8 @@ import { Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { EnvInterface } from '@guitar/interfaces';
-import { PedalUsage } from './../../../../../apps/guitar-api/src/app/schemas/pedal-usage.schema';
 
+// TODO: This service might not be needed
 @Injectable({
   providedIn: 'root',
 })
@@ -15,11 +15,11 @@ export class PedalUsageService {
     @Inject('environment') private env: EnvInterface
   ) {}
 
-  getPedalUsage(): Observable<PedalUsage[]> {
-    return this.http.get<PedalUsage[]>(`${this.apiUrl}`);
+  getPedalUsage(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}`);
   }
 
-  createPedalUsage(pedalUsageData: any): Observable<PedalUsage> {
-    return this.http.post<PedalUsage>(`${this.apiUrl}`, pedalUsageData);
+  createPedalUsage(pedalUsageData: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}`, pedalUsageData);
   }
 }
