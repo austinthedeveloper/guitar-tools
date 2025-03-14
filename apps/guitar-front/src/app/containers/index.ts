6d9468a4e92f93c06b8095450db506073f04f205
Austin Stewart <astewart@texasdevs.com>
2025-03-14 15:46:06 -0500

    Removed all files from the guitar-app

--- apps/guitar-front/src/app/containers/index.ts
+++ apps/guitar-front/src/app/containers/index.ts
@@ -1,12 +1,8 @@
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
 import { MetronomeComponent } from './metronome/metronome.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { OptionsComponent } from './options/options.component';
 import { QuizComponent } from './quiz/quiz.component';
-import { SetupDashboardComponent } from './setup/components/setup-dashboard/setup-dashboard.component';
-import { SetupPairingDetailComponent } from './setup/components/setup-pairing-detail/setup-pairing-detail.component';
-import { SetupComponent } from './setup/setup.component';
 
 export const APP_CONTAINERS = [
   HomeComponent,
@@ -14,8 +10,4 @@ export const APP_CONTAINERS = [
   NavbarComponent,
   OptionsComponent,
   MetronomeComponent,
-  LoginComponent,
-  SetupComponent,
-  SetupDashboardComponent,
-  SetupPairingDetailComponent,
 ];
