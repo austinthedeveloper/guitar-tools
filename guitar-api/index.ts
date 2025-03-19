0747e109fed6c63592a0ff023ef560025494545d
Austin Stewart <astewart@texasdevs.com>
2025-03-18 23:48:02 -0500

    Small config updates

--- guitar-api/index.ts
+++
@@ -1,7 +0,0 @@
-import { HttpRequest, Context } from '@azure/functions';
-import { AzureHttpAdapter } from '@nestjs/azure-func-http';
-import { createApp } from '../apps/guitar-api/src/main.azure';
-
-export default function (context: Context, req: HttpRequest): void {
-  AzureHttpAdapter.handle(createApp, context, req);
-}
