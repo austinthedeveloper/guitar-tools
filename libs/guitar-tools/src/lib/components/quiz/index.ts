472890ceeba334f877b08ef09aa70df3118dec90
Austin Stewart <astewart@texasdevs.com>
2025-10-17 16:59:52 -0500

    tsconfig and barel

--- libs/guitar-tools/src/lib/components/quiz/index.ts
+++ libs/guitar-tools/src/lib/components/quiz/index.ts
@@ -9,6 +9,19 @@ import { ScaleQuizComponent } from './scale-quiz/scale-quiz.component';
 import { SpecificNoteComponent } from './specific-note/specific-note.component';
 import { TriadNoteOrderComponent } from './triad-note-order/triad-note-order.component';
 
+export * from './chord-quiz/chord-quiz.component';
+export * from './fretboard-quiz/fretboard-quiz.component';
+export * from './mode-name-quiz/mode-name-quiz.component';
+export * from './mode-ordering-quiz/mode-ordering-quiz.component';
+export * from './mode-quiz/mode-quiz.component';
+export * from './quiz-base/quiz-base.component';
+export * from './quiz-totals/quiz-totals.component';
+export * from './relative-major-quiz/relative-major-quiz.component';
+export * from './relative-minor-quiz/relative-minor-quiz.component';
+export * from './scale-quiz/scale-quiz.component';
+export * from './specific-note/specific-note.component';
+export * from './triad-note-order/triad-note-order.component';
+
 export const GUITAR_QUIZ_COMPONENTS = [
   ChordQuizComponent,
   ModeNameQuizComponent,
